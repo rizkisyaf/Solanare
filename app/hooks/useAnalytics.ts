acf076de988cfdd008f019a7eacfa1353bff408d
rizkisyaf <rizkisyaf@gmail.com>
2024-12-12 22:39:56 +0700

    Refactor useAnalytics hook to simplify tracking function. Removed unnecessary wrapper around the track function from @vercel/analytics, enhancing code clarity and maintainability. This change aligns with the goal of streamlining analytics integration.

--- app/hooks/useAnalytics.ts
+++ app/hooks/useAnalytics.ts
@@ -1,9 +1,5 @@
-import { track as vercelTrack } from '@vercel/analytics';
+import { track } from '@vercel/analytics';
 
 export function useAnalytics() {
-  const track = (eventName: string, properties?: Record<string, any>, flags?: string[]) => {
-    vercelTrack(eventName, properties, { flags });
-  };
-
   return { track };
 } 
